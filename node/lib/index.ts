808cd7284c956c5b49abe3997f3622b0e4d91d5f
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-07-17 17:29:51 +0200

    fix(node): remove ad-hoc test from index.ts

--- node/lib/index.ts
+++ node/lib/index.ts
@@ -11,15 +11,3 @@ export const getIcp = async (): Promise<string> => {
 export const getIdFromEvent = async (event: string): Promise<string> => {
     return await addon.getIdFromEvent(event);
 }
-
-getIcp().then(async (icp) => {
-    const jicp = JSON.parse(icp);
-    const [sev, sigs_str] = jicp.icp.split("\r\n\r\n");
-    const sigs = sigs_str.split("\n");
-    const ev = JSON.parse(sev);
-    console.log(ev)
-    console.log(sigs)
-    const sa = JSON.stringify([jicp.icp])
-    console.log(sa)
-    console.log(await validateEvents(sa))
-})
