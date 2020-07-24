9b0d5de1aac4c1e48c7226dc9e3f66fb360f0ff5
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-07-24 13:08:54 +0200

    feat(wallet): expose wallet fns to neon/js

--- node/lib/index.ts
+++ node/lib/index.ts
@@ -1,5 +1,16 @@
 var addon = require('../native');
 
+export const enum KeyTypes = {
+    jwsVerificationKey2020 = "JwsVerificationKey2020",
+    ecdsaSecp256k1VerificationKey2019 = "EcdsaSecp256k1VerificationKey2019",
+    ed25519VerificationKey2018 = "Ed25519VerificationKey2018",
+    gpgVerificationKey2020 = "GpgVerificationKey2020",
+    rsaVerificationKey2018 = "RsaVerificationKey2018",
+    x25519KeyAgreementKey2019 = "X25519KeyAgreementKey2019",
+    schnorrSecp256k1VerificationKey2019 = "SchnorrSecp256k1VerificationKey2019",
+    ecdsaSecp256k1RecoveryMethod2020 = "EcdsaSecp256k1RecoveryMethod2020",
+}
+
 export const validateEvents = async (events: string): Promise<string> => {
     return await addon.validateEvents(events);
 }
@@ -10,4 +21,29 @@ export const getIcp = async (): Promise<string> => {
 
 export const getIdFromEvent = async (event: string): Promise<string> => {
     return await addon.getIdFromEvent(event);
+}
+
+export const newWallet = async (id: string, pass: string): Promise<string> => {
+    return await addon.newWallet(id, pass);
+}
+
+export const changePass = async (encryptedWallet: string, id: string, oldPass: string,
+                                newPass: string): Promise<string> => {
+    return await addon.changePass(encryptedWallet, id, oldPass, newPass);
+}
+
+export const newKey = async (encryptedWallet: string, id: string, pass: string, keyType: KeyTypes): Promise<string> => {
+    return await addon.newKey(encryptedWallet, id, pass, keyType)
+}
+
+export const getKeys = async (encryptedWallet: string, id: string, pass: string): Promise<string> => {
+    return await addon.getKeys(encryptedWallet, id, pass)
+}
+
+export const sign = async (encryptedWallet: string, id: string, pass: string, data: string, key_ref: string): Promise<string> => {
+    return await addon.sign(encryptedWallet, id, pass, data, key_ref)
+}
+
+export const verify = async (encryptedWallet: string, id: string, pass: string, data: string, key_ref: string, signature: string): Promise<string> => {
+    return await addon.verify(encryptedWallet, id, pass, data, key_ref, signature)
 }
--- node/native/src/lib.rs
+++ node/native/src/lib.rs
@@ -1,4 +1,4 @@
-use jolocom_native_utils::{get_icp_str, get_id_from_event_str, validate_events_str};
+use jolocom_native_utils::{get_icp_str, get_id_from_event_str, validate_events_str, wallet};
 use neon::prelude::*;
 
 fn get_icp(mut cx: FunctionContext) -> JsResult<JsString> {
@@ -15,9 +15,63 @@ fn get_id_from_event(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string(get_id_from_event_str(str)))
 }
 
+fn new_wallet(mut cx: FunctionContext) -> JsResult<JsString> {
+    let id = cx.argument::<JsString>(0)?.value();
+    let pass = cx.argument::<JsString>(1)?.value();
+    Ok(cx.string(wallet::new_wallet(id, pass)))
+}
+
+fn change_pass(mut cx: FunctionContext) -> JsResult<JsString> {
+    let ew = cx.argument::<JsString>(0)?.value();
+    let id = cx.argument::<JsString>(1)?.value();
+    let old_pass = cx.argument::<JsString>(2)?.value();
+    let new_pass = cx.argument::<JsString>(3)?.value();
+    Ok(cx.string(wallet::change_pass(ew, id, old_pass, new_pass)))
+}
+
+fn new_key(mut cx: FunctionContext) -> JsResult<JsString> {
+    let ew = cx.argument::<JsString>(0)?.value();
+    let id = cx.argument::<JsString>(1)?.value();
+    let pass = cx.argument::<JsString>(2)?.value();
+    let key_type = cx.argument::<JsString>(3)?.value();
+    Ok(cx.string(wallet::new_key(ew, id, pass, key_type)))
+}
+
+fn get_keys(mut cx: FunctionContext) -> JsResult<JsString> {
+    let ew = cx.argument::<JsString>(0)?.value();
+    let id = cx.argument::<JsString>(1)?.value();
+    let pass = cx.argument::<JsString>(2)?.value();
+    Ok(cx.string(wallet::get_keys(ew, id, pass)))
+}
+
+fn sign(mut cx: FunctionContext) -> JsResult<JsString> {
+    let ew = cx.argument::<JsString>(0)?.value();
+    let id = cx.argument::<JsString>(1)?.value();
+    let pass = cx.argument::<JsString>(2)?.value();
+    let data = cx.argument::<JsString>(3)?.value();
+    let key_ref = cx.argument::<JsString>(4)?.value();
+    Ok(cx.string(wallet::sign(ew, id, pass, data, key_ref)))
+}
+
+fn verify(mut cx: FunctionContext) -> JsResult<JsBoolean> {
+    let ew = cx.argument::<JsString>(0)?.value();
+    let id = cx.argument::<JsString>(1)?.value();
+    let pass = cx.argument::<JsString>(2)?.value();
+    let data = cx.argument::<JsString>(3)?.value();
+    let key_ref = cx.argument::<JsString>(4)?.value();
+    let sig = cx.argument::<JsString>(4)?.value();
+    Ok(cx.boolean(wallet::verify(ew, id, pass, data, key_ref, sig)))
+}
+
 register_module!(mut cx, {
     cx.export_function("getIcp", get_icp)?;
     cx.export_function("validateEvents", validate_events)?;
     cx.export_function("getIdFromEvent", get_id_from_event)?;
+    cx.export_function("newWallet", new_wallet)?;
+    cx.export_function("changePass", change_pass)?;
+    cx.export_function("newKey", new_key)?;
+    cx.export_function("getKeys", get_keys)?;
+    cx.export_function("sign", sign)?;
+    cx.export_function("verify", verify)?;
     Ok(())
 });
