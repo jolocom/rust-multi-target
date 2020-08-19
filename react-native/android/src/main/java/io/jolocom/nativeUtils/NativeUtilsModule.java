412cbd9ce8275f67cbe540d8c7453256c4913012
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-08-20 00:27:24 +0200

    feat(android): update android ffi with wallet utils

--- react-native/android/src/main/java/io/jolocom/nativeUtils/NativeUtilsModule.java
+++ react-native/android/src/main/java/io/jolocom/nativeUtils/NativeUtilsModule.java
@@ -38,16 +38,6 @@ public class NativeUtilsModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void getIcp(Promise promise) {
-    try {
-      String result = getIcpStr();
-      promise.resolve(result);
-    } catch (Exception e) {
-      rejectWithException(promise, "generating the ICP", e);
-    }
-  }
-
-  @ReactMethod
   public void extractIdFromEvent(String event, Promise promise) {
     try {
       String result = extractIdFromEventStr(event);
@@ -57,7 +47,197 @@ public class NativeUtilsModule extends ReactContextBaseJavaModule {
     }
   }
 
+  @ReactMethod
+  public void newWallet(String id, String pass, Promise promise) {
+    try {
+      String result = newWalletStr(id, pass);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "creating an empty wallet", e);
+    }
+  }
+
+  @ReactMethod
+  public void keriInceptWallet(String ew, String id, String pass,
+                               Promise promise) {
+    try {
+      String result = keriInceptWalletStr(ew, id, pass);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "incepting a KERI identity with a wallet",
+                          e);
+    }
+  }
+
+  @ReactMethod
+  public void changePass(String ew, String id, String oldPass,
+                         String newPass Promise promise) {
+    try {
+      String result = changePassStr(ew, id, oldPass, newPass);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "changing a wallet password", e);
+    }
+  }
+
+  @ReactMethod
+  public void changeId(String ew, String id, String newId, String pass,
+                       Promise promise) {
+    try {
+      String result = changeIdStr(ew, id, newId, pass);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "changing a wallet ID", e);
+    }
+  }
+
+  @ReactMethod
+  public void newKey(String ew, String id, String pass, String keyType,
+                     String controller, Promise promise) {
+    try {
+      String result = newKeyStr(ew, id, pass, keyType, controller);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "adding a new key pair to a wallet", e);
+    }
+  }
+
+  @ReactMethod
+  public void addContent(String ew, String id, String pass, String content,
+                         Promise promise) {
+    try {
+      String result = addContentStr(ew, id, pass, content);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "adding content to a wallet", e);
+    }
+  }
+
+  @ReactMethod
+  public void setKeyController(String ew, String id, String pass, String keyRef,
+                               String controller, Promise promise) {
+    try {
+      String result = setKeyControllerStr(ew, id, pass, keyRef, controller);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "setting a public key controller", e);
+    }
+  }
+
+  @ReactMethod
+  public void getKey(String ew, String id, String pass, Promise promise) {
+    try {
+      String result = getKeyStr(event);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "getting a public key", e);
+    }
+  }
+
+  @ReactMethod
+  public void getKeyByController(String ew, String id, String pass,
+                                 String controller, Promise promise) {
+    try {
+      String result = getKeyByControllerStr(ew, id, pass, controller);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "getting a public key", e);
+    }
+  }
+
+  @ReactMethod
+  public void getKeys(String ew, String id, String pass, Promise promise) {
+    try {
+      String result = getKeysStr(ew, id, pass);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "getting public keys", e);
+    }
+  }
+
+  @ReactMethod
+  public void sign(String ew, String id, String pass, String data,
+                   String controller, Promise promise) {
+    try {
+      String result = signStr(ew, id, pass, data, controller);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "signing a message", e);
+    }
+  }
+
+  @ReactMethod
+  public void verify(String key, String keyType, String data, String signature,
+                     Promise promise) {
+    try {
+      Bool result = verifyStr(key, keyType, data, signature);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "verifying a signature", e);
+    }
+  }
+
+  @ReactMethod
+  public void encrypt(String pkInfo, String data, String aad, Promise promise) {
+    try {
+      String result = encryptStr(pkInfo, data, aad);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "encrypting a message", e);
+    }
+  }
+
+  @ReactMethod
+  public void decrypt(String ew, String id, String pass, String keyRef,
+                      String aad, Promise promise) {
+    try {
+      String result = decryptStr(ew, id, pass, keyRef, aad);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "decrypting a message", e);
+    }
+  }
+
+  @ReactMethod
+  public void getRandom(UInt len) {
+    try {
+      String result = getRandomStr(len);
+      promise.resolve(result);
+    } catch (Exception e) {
+      rejectWithException(promise, "generating random bytes", e);
+    }
+  }
+
   private static native String validateEventsStr(String events);
   private static native String extractIdFromEventStr(String event);
-  private static native String getIcpStr();
+  private static native String newWalletStr(String id, String pass);
+  private static native String keriInceptWalletStr(String ew, String id,
+                                                   String pass);
+  private static native String changePassStr(String ew, String id,
+                                             String oldPass, String newPass);
+  private static native String changeIdStr(String ew, String id, String newId,
+                                           String pass);
+  private static native String newKeyStr(String ew, String id, String pass,
+                                         String keyType, String controller);
+  private static native String addContentStr(String ew, String id, String pass,
+                                             String content);
+  private static native String setKeyControllerStr(String ew, String id,
+                                                   String pass, String keyRef,
+                                                   String controller);
+  private static native String getKeyStr(String ew, String id, String pass,
+                                         String keyRef);
+  private static native String getKeyByControllerStr(String ew, String id,
+                                                     String pass,
+                                                     String controller);
+  private static native String getKeysStr(String ew, String id, String pass);
+  private static native String signStr(String ew, String id, String pass,
+                                       String data, String controller);
+  private static native String verifyStr(String key, String keyType,
+                                         String data, String signature);
+  private static native String encryptStr(String pkInfo, String data,
+                                          String aad);
+  private static native String decryptStr(String ew, String id, String pass,
+                                          String keyRef, String data,
+                                          String aad);
+  private static native String getRandomStr(String ew, String id, String pass);
 }
--- react-native/rust/keriox_wrapper/src/lib.rs
+++ react-native/rust/keriox_wrapper/src/lib.rs
@@ -1,24 +1,91 @@
 mod export;
-use jolocom_native_utils::{
-    get_icp_str,
-    validate_events_str,
-    get_id_from_event_str
-};
+use jolocom_native_utils::{get_id_from_event_str, validate_events_str, wallet};
+use std::convert::TryInto;
 
 export! {
-    @Java_io_jolocom_nativeUtils_NativeUtilsModule_getIcpStr
-    // returns a signed versioned key event with the keys
-    fn get_icp() -> String {
-        get_icp_str()
-    }
-
     @Java_io_jolocom_nativeUtils_NativeUtilsModule_validateEventsStr
     fn validate_events(kel_string: String) -> String {
-        validate_events_str(kel_string)
+        validate_events_str(&kel_string)
     }
 
     @Java_io_jolocom_nativeUtils_NativeUtilsModule_extractIdFromEventStr
     fn get_id_from_event(event_string: String) -> String {
-        get_id_from_event_str(event_string)
+        get_id_from_event_str(&event_string)
     }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_newWalletStr
+    fn new_wallet(id: String, pass: String) -> String {
+        wallet::new_wallet(&id, &pass)
+    }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_keriInceptWalletStr
+    fn keri_incept_wallet(ew: String, id: String, pass: String) -> String {
+        wallet::incept_wallet(&ew, &id, &pass)
+    }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_changePassStr
+    fn change_pass(ew: String, id: String, old_pass: String, new_pass: String) -> String {
+        wallet::change_pass(&ew, &id, &old_pass, &new_pass)
+    }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_changeIdStr
+    fn change_id(ew: String, id: String, new_id: String, pass: String) -> String {
+        wallet::change_pass(&ew, &id, &new_id, &pass)
+    }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_newKeyStr
+    fn new_key(ew: String, id: String, pass: String, key_type: String, controller: String) -> String {
+        wallet::new_key(&ew, &id, &pass, &key_type, if controller.len() > 0 {Some(vec![controller])} else { None })
+    }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_addContentStr
+    fn add_content(ew: String, id: String, pass: String, content: String) -> String {
+        wallet::add_content(&ew, &id, &pass, &content)
+    }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_setKeyControllerStr
+    fn set_key_controller(ew: String, id: String, pass: String, key_ref: String, controller: String) -> String {
+        wallet::set_key_controller(&ew, &id, &pass, &key_ref, &controller)
+    }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_getKeyStr
+    fn get_key(ew: String, id: String, pass: String, key_ref: String) -> String {
+        wallet::get_key(&ew, &id, &pass, &key_ref)
+    }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_getKeyByControllerStr
+    fn get_key_by_controller(ew: String, id: String, pass: String, controller: String) -> String {
+        wallet::get_key_by_controller(&ew, &id, &pass, &controller)
+    }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_getKeysStr
+    fn get_keys(ew: String, id: String, pass: String) -> String {
+        wallet::get_keys(&ew, &id, &pass)
+    }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_signStr
+    fn sign(ew: String, id: String, pass: String, data: String, controller: String) -> String {
+        wallet::sign(&ew, &id, &pass, &data, &controller)
+    }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_verifyStr
+    fn verify(key: String, key_type: String, data: String, signature: String) -> bool {
+        wallet::verify(&key, &key_type, &data, &signature)
+    }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_encryptStr
+    fn encrypt(pk_info: String, data: String, aad: String) -> String {
+        wallet::encrypt(&pk_info, &data, &aad)
+    }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_decryptStr
+    fn decrypt(ew: String, id: String, pass: String, key_ref: String, data: String, aad: String) -> String {
+        wallet::decrypt(&ew, &id, &pass, &key_ref, &data, &aad)
+    }
+
+    @Java_io_jolocom_nativeUtils_NativeUtilsModule_getRandomStr
+    fn get_random(len: u64) -> String {
+        wallet::get_random_b64(len.try_into().unwrap())
+    }
+
 }
