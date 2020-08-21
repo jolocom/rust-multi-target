f1389a44c8c4f0a0c86e1eebdabebc60a64d09a1
mnzaki <mnzaki@gmail.com>
2020-08-21 17:54:56 +0200

    fix(prepare-platform-arch-pkg): logging and copy more files

--- node/prepare-platform-arch-pkg.js
+++ node/prepare-platform-arch-pkg.js
@@ -14,8 +14,6 @@ const tmplPkgJsonPath = path.join(baseDir, 'package.json')
 const outDir = path.resolve(__dirname, 'platform-arch-pkg')
 const outPkgJsonPath = path.join(outDir, 'package.json')
 
-fs.removeSync(outDir)
-fs.mkdirpSync(outDir)
 const pkgJson = JSON.parse(fs.readFileSync(tmplPkgJsonPath).toString())
 
 const platformArch = `${process.platform}-${process.arch}`
@@ -24,25 +22,38 @@ pkgJson.os = [process.platform]
 pkgJson.name += `-${platformArch}`
 pkgJson.description += ` [${platformArch}]`
 
+console.log(
+  '\n\n\n' +
+  'Generating platform specific package: ' + pkgJson.name
+)
+
 // Remove properties we don't need in the package.json file
 // for the native packages
 delete pkgJson.optionalDependencies
 delete pkgJson.scripts
 
 // add the 'index.node' binary file to the packaged files
-pkgJson.files.push('native/index.node')
+pkgJson.files.push('native/index.node', 'README', 'LICENSE', 'CONTRIBUTING')
+
+// clear out the output directory
+fs.removeSync(outDir)
+fs.mkdirpSync(outDir)
 
 // write out platform-arch-pkg/package.json
 fs.writeFileSync(outPkgJsonPath, JSON.stringify(pkgJson, null, 2))
+
 // and copy over all files necessary for packaging
 pkgJson.files.forEach(p => {
-  const outPath = path.join(outDir, p)
-  fs.copySync(p, outPath)
+  try {
+    console.log('copying ' + p)
+    const outPath = path.join(outDir, p)
+    fs.copySync(p, outPath)
+  } catch (err) {
+    console.error('could not copy ', p)
+  }
 })
 
-
 console.log(
-  '\n\n\n' +
-  'Generated ' + outDir +
+  'Generated at ' + outDir +
   '\n\n\n'
 )
