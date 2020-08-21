42a53976b60b7e1ffc5c1957bcbdcb72c8d4ece2
mnzaki <mnzaki@gmail.com>
2020-08-21 17:45:30 +0200

    fix: don't include binary in top package

--- node/prepare-platform-arch-pkg.js
+++ node/prepare-platform-arch-pkg.js
@@ -1,3 +1,9 @@
+/**
+ * This script will create a platform specific package
+ * such as '@jolocom/native-core-node-linux-x64' using the
+ * parent directory's package.json and build output
+ */
+
 const path = require('path')
 const fs = require('fs-extra')
 
@@ -18,10 +24,17 @@ pkgJson.os = [process.platform]
 pkgJson.name += `-${platformArch}`
 pkgJson.description += ` [${platformArch}]`
 
+// Remove properties we don't need in the package.json file
+// for the native packages
 delete pkgJson.optionalDependencies
 delete pkgJson.scripts
 
+// add the 'index.node' binary file to the packaged files
+pkgJson.files.append('native/index.node')
+
+// write out platform-arch-pkg/package.json
 fs.writeFileSync(outPkgJsonPath, JSON.stringify(pkgJson, null, 2))
+// and copy over all files necessary for packaging
 pkgJson.files.forEach(p => {
   const outPath = path.join(outDir, p)
   fs.copySync(p, outPath)
