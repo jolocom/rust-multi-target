96798ad6ea3d2d63e50f7c08d1c338d24b40c782
mnzaki <mnzaki@gmail.com>
2020-08-24 11:19:36 +0200

    fix(node): load local build while devving

--- node/lib/addon.ts
+++ node/lib/addon.ts
@@ -1,1 +1,7 @@
-module.exports = require(`@jolocom/native-core-node-${process.platform}-${process.arch}`);
+try {
+  // during development, use the build that's directly here
+  module.exports = require('../native')
+  console.warn('@jolocom/native-core: using native build from main package!')
+} catch {
+  module.exports = require(`@jolocom/native-core-node-${process.platform}-${process.arch}`);
+}
