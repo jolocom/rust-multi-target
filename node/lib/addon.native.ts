8e8901e9d235b9b94001c7c540717e4134ea8d9b
mnzaki <mnzaki@gmail.com>
2020-08-27 20:14:58 +0200

    fix(react-native): NativeModule now called JolocomCore

--- node/lib/addon.native.ts
+++ node/lib/addon.native.ts
@@ -2,4 +2,4 @@
  * This file is loaded by the metro bundler (React Native) when importing
  * './addon' as it ends in `.native.ts`
  */
-module.exports = require('react-native').NativeModules.NativeUtils
+module.exports = require('react-native').NativeModules.JolocomCore
